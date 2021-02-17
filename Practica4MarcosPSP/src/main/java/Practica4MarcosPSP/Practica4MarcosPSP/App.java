1b9f6fd84189e8b7cd50e33433d52abf6d69d380
ManuelRevuelto <manu.revuelto1111@gmail.com>
2021-02-17 16:45:57 +0100

    Primer Commit

---
+++ Practica4MarcosPSP/src/main/java/Practica4MarcosPSP/Practica4MarcosPSP/App.java
@@ -0,0 +1,13 @@
+package Practica4MarcosPSP.Practica4MarcosPSP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Practica4MarcosPSP/src/test/java/Practica4MarcosPSP/Practica4MarcosPSP/AppTest.java
@@ -0,0 +1,20 @@
+package Practica4MarcosPSP.Practica4MarcosPSP;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
