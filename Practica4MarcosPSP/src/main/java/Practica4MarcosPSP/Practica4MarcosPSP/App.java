08b78d7cb2f379e43167fc209001347166727f74
ManuelRevuelto <manu.revuelto1111@gmail.com>
2021-03-02 00:48:30 +0100

    nada

--- Practica4MarcosPSP/src/main/java/Practica4MarcosPSP/Practica4MarcosPSP/App.java
+++ Practica4MarcosPSP/src/main/java/Practica4MarcosPSP/Practica4MarcosPSP/App.java
@@ -1,13 +1,25 @@
 package Practica4MarcosPSP.Practica4MarcosPSP;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+public class App {
+	
+	final String SERVIDOR = "demo.wtfpserver.com";
+	final int PUERTO = 21;
+	final String USUARIO = "demo-user";
+	final String PASSWORD = "demo-user";
+	
+	public static void main(String[] args) {
+		
+		/* FTPClient client = new FTPClient(); */
+
+		String sFTP = "ftp.miservidor.com";
+		String sUser = "usuario";
+		String sPassword = "password";
+				
+		/*
+		 * try { client.connect(sFTP); boolean login = client.login(sUser,sPassword); }
+		 * catch (IOException ioe) {}
+		 */
+	}
 }
